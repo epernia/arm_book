edb4712400ddfc97b16d6f0737e9b705b18f84ab
elpablogomez <elpablogomez@gmail.com>
2020-06-24 16:12:58 -0300

    Se suben los examples del chapter 4

---
+++ programs/chapter_04/example_4_3_with_proposed_exercise_1.cpp
@@ -0,0 +1,391 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+#define NUMBER_OF_KEYS     4
+#define STRING_MAX_LENGTH 30
+#define POTENTIOMETER_OVER_TEMP_LEVEL 50
+
+//=====[Declaration and intitalization of public global objects]===============
+
+DigitalIn enterButton(BUTTON1);
+DigitalIn gasDetector(D2);
+DigitalIn aButton(D4);
+DigitalIn bButton(D5);
+DigitalIn cButton(D6);
+DigitalIn dButton(D7);
+
+AnalogIn potentiometer(A0);
+
+DigitalOut alarmLed(LED1);
+DigitalOut incorrectCodeLed(LED3);
+DigitalOut systemBlockedLed(LED2);
+
+Serial uartUsb(USBTX, USBRX);
+Serial uartBle(D1, D0);
+
+//=====[Declaration and intitalization of public global variables]=============
+
+bool alarmState    = OFF;
+bool incorrectCode = false;
+bool overTempDetector = OFF;
+
+int numberOfIncorrectCodes = 0;
+int buttonBeingCompared    = 0;
+int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 };
+int buttonsPressed[NUMBER_OF_KEYS] = { 0, 0, 0, 0 };
+
+char receivedChar = '\0';
+char bleReceivedString[STRING_MAX_LENGTH];
+
+bool alarmLastTransmittedState = OFF;
+bool gasLastTransmittedState   = OFF;
+bool tempLastTransmittedState  = OFF;
+bool ICLastTransmittedState    = OFF;
+bool SBLastTransmittedState    = OFF;
+
+float potentiometerReading;
+float potentiometerReadingScaled;
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void alarmActivationUpdate();
+void alarmDeactivationUpdate();
+
+void uartTask();
+void availableCommands();
+bool areEqual();
+void bleTask();
+
+void bleSendElementStateToTheSmartphone( bool lastTransmittedState, 
+                                         bool currentState, 
+                                         char* elementName );
+
+void bleGetTheSmartphoneButtonsState( char* buttonName, int index );
+
+float celsiusToFahrenheit( float tempInCelsiusDegrees );
+float analogReadingScaledWithTheLM35Formula( float analogReading );
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    inputsInit();
+    outputsInit();
+    while (true) {
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
+        uartTask();
+        bleTask();
+    }
+}         
+
+//=====[Implementations of public functions]===================================
+
+void inputsInit()
+{
+    gasDetector.mode(PullDown);
+    aButton.mode(PullDown);
+    bButton.mode(PullDown);
+    cButton.mode(PullDown);
+    dButton.mode(PullDown);
+}
+
+void outputsInit()
+{
+    alarmLed = OFF;
+    incorrectCodeLed = OFF;
+    systemBlockedLed = OFF;
+}
+
+void alarmActivationUpdate()
+{
+    potentiometerReadingScaled = 
+		analogReadingScaledWithTheLM35Formula( potentiometer.read() );
+    
+    if ( potentiometerReadingScaled > POTENTIOMETER_OVER_TEMP_LEVEL ) {
+        overTempDetector = ON;
+    } else {
+        overTempDetector = OFF;
+    }
+    
+    if ( gasDetector || overTempDetector ) {
+        alarmState = ON;
+    }
+    alarmLed = alarmState;
+}
+
+void alarmDeactivationUpdate()
+{
+    if ( numberOfIncorrectCodes < 5 ) {
+        if ( aButton && bButton && cButton && dButton && !enterButton ) {
+            incorrectCodeLed = OFF;
+        }
+        if ( enterButton && !incorrectCodeLed && alarmState ) {
+            buttonsPressed[0] = aButton;
+            buttonsPressed[1] = bButton;
+            buttonsPressed[2] = cButton;
+            buttonsPressed[3] = dButton;
+            if ( areEqual() ) {
+                alarmState = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes++;
+            }
+        }
+    } else {
+        systemBlockedLed = ON;
+    }
+}
+
+void uartTask()
+{
+    if( uartUsb.readable() ) {
+        char receivedChar = uartUsb.getc();
+        switch (receivedChar) {
+        case '1':
+            if ( alarmState ) {
+                uartUsb.printf( "The alarmLed is activated\r\n");
+            } else {
+                uartUsb.printf( "The alarmLed is not activated\r\n");
+            }
+            break;
+
+        case '2':
+            if ( gasDetector ) {
+                uartUsb.printf( "Gas is being detected\r\n");
+            } else {
+                uartUsb.printf( "Gas is not being detected\r\n");
+            }
+            break;
+
+        case '3':
+            if ( overTempDetector ) {
+                uartUsb.printf( "Temperature is above the maximum level\r\n");
+            } else {
+                uartUsb.printf( "Temperature is below the maximum level\r\n");
+            }
+            break;
+            
+        case '4':
+            uartUsb.printf( "Please enter the code sequence.\r\n" );
+            uartUsb.printf( "First enter 'A', then 'B', then 'C', and " ); 
+            uartUsb.printf( "finally 'D' button\r\n" );
+            uartUsb.printf( "In each case type 1 for pressed or 0 for " );
+            uartUsb.printf( "not pressed\r\n" );
+            uartUsb.printf( "For example, for 'A' = pressed, " );
+            uartUsb.printf( "'B' = pressed, 'C' = not pressed, ");
+            uartUsb.printf( "'D' = not pressed, enter '1', then '1', " );
+            uartUsb.printf( "then '0', and finally '0'\r\n\r\n" );
+
+            incorrectCode = false;
+
+            for ( buttonBeingCompared = 0; 
+                  buttonBeingCompared < NUMBER_OF_KEYS; 
+                  buttonBeingCompared++) {
+
+                receivedChar = uartUsb.getc();
+
+                if ( receivedChar == '1' ) {
+                    if ( codeSequence[buttonBeingCompared] != 1 ) {
+                        incorrectCode = true;
+                    }
+                } else if ( receivedChar == '0' ) {
+                    if ( codeSequence[buttonBeingCompared] != 0 ) {
+                        incorrectCode = true;
+                    }
+                }
+            }
+
+            if ( incorrectCode == false ) {
+                uartUsb.printf( "The code is correct\r\n\r\n" );
+                alarmState = OFF;
+                incorrectCodeLed = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                uartUsb.printf( "The code is incorrect\r\n\r\n" );
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
+            }                
+            break;
+
+        case '5':
+            uartUsb.printf( "Please enter new code sequence\r\n" );
+            uartUsb.printf( "First enter 'A', then 'B', then 'C', and " );
+            uartUsb.printf( "finally 'D' button\r\n" );
+            uartUsb.printf( "In each case type 1 for pressed or 0 for not " );
+            uartUsb.printf( "pressed\r\n" );
+            uartUsb.printf( "For example, for 'A' = pressed, 'B' = pressed," );
+            uartUsb.printf( " 'C' = not pressed," );
+            uartUsb.printf( "'D' = not pressed, enter '1', then '1', " );
+            uartUsb.printf( "then '0', and finally '0'\r\n\r\n" );
+
+            for ( buttonBeingCompared = 0; 
+                  buttonBeingCompared < NUMBER_OF_KEYS; 
+                  buttonBeingCompared++) {
+
+                receivedChar = uartUsb.getc();
+
+                if ( receivedChar == '1' ) {
+                    codeSequence[buttonBeingCompared] = 1;
+                } else if ( receivedChar == '0' ) {
+                    codeSequence[buttonBeingCompared] = 0;
+                }
+            }
+
+            uartUsb.printf( "New code generated\r\n\r\n" );
+            break;
+            
+        case 'p':
+        case 'P':
+            potentiometerReading = potentiometer.read();
+            uartUsb.printf( "Potentiometer: %.2f\r\n", potentiometerReading );
+            break;
+
+        case 'c':
+        case 'C':
+            uartUsb.printf( "Temperature: %.2f °C\r\n", 
+				analogReadingScaledWithTheLM35Formula
+				( potentiometer.read() ) );
+            break;
+
+        case 'f':
+        case 'F':
+            uartUsb.printf( "Temperature: %.2f °F\r\n", 
+				celsiusToFahrenheit 
+				( analogReadingScaledWithTheLM35Formula 
+				( potentiometer.read() ) ) );
+            break;
+
+        default:
+            availableCommands();
+            break;
+
+        }
+    }
+}
+
+void availableCommands()
+{
+    uartUsb.printf( "Available commands:\r\n" );
+    uartUsb.printf( "Press '1' to get the alarm state\r\n" );
+    uartUsb.printf( "Press '2' for gas detector state\r\n" );
+    uartUsb.printf( "Press '3' for over temperature detector state\r\n" );
+    uartUsb.printf( "Press '4' to enter the code sequence\r\n" );
+    uartUsb.printf( "Press '5' to enter a new code\r\n" );
+    uartUsb.printf( "Press 'P' or 'p' to get potentiometer reading\r\n\r\n" );
+	uartUsb.printf( "Press 'f' or 'F' to get potentiometer reading in Fahrenheit\r\n" );
+	uartUsb.printf( "Press 'c' or 'C' to get potentiometer reading in Celcius\r\n\r\n" );
+}
+
+bool areEqual()
+{
+    int i;
+
+    for (i = 0; i < NUMBER_OF_KEYS; i++) {
+        if (codeSequence[i] != buttonsPressed[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void bleTask()
+{
+    bleSendElementStateToTheSmartphone( alarmLastTransmittedState, 
+                                        alarmState, "ALARM" );
+    alarmLastTransmittedState = alarmState;
+
+    bleSendElementStateToTheSmartphone( gasLastTransmittedState, 
+                                        gasDetector, "GAS_DET" );
+    gasLastTransmittedState = gasDetector;
+
+    bleSendElementStateToTheSmartphone( tempLastTransmittedState, 
+                                        overTempDetector, "OVER_TEMP" );
+    tempLastTransmittedState = overTempDetector;
+
+    bleSendElementStateToTheSmartphone( ICLastTransmittedState, 
+                                        incorrectCodeLed, "LED_IC" );
+    ICLastTransmittedState = incorrectCodeLed;
+
+    bleSendElementStateToTheSmartphone( SBLastTransmittedState, 
+                                        systemBlockedLed, "LED_SB" );
+    SBLastTransmittedState = systemBlockedLed;
+
+    if( uartBle.readable() ) {
+        uartBle.scanf("%s", bleReceivedString);
+
+        bleGetTheSmartphoneButtonsState( "A", 0 );
+        bleGetTheSmartphoneButtonsState( "B", 1 );
+        bleGetTheSmartphoneButtonsState( "C", 2 );
+        bleGetTheSmartphoneButtonsState( "D", 3 );
+
+        bleGetTheSmartphoneButtonsState( "ENTER", -1 );
+
+        while ( uartBle.readable() ) {
+            uartBle.getc();
+        }
+    } 
+    
+}
+
+void bleSendElementStateToTheSmartphone( bool lastTransmittedState, 
+                                         bool currentState, 
+                                         char* elementName )
+{
+    if ( lastTransmittedState != currentState ) {
+        uartBle.printf(elementName);
+        if ( currentState ) {
+            uartBle.printf("_ON\r\n");
+        } else {
+            uartBle.printf("_OFF\r\n");
+        }
+    }
+}
+
+void bleGetTheSmartphoneButtonsState( char* buttonName, int index )
+{
+    char str[30];
+
+    str[0] = 0;
+    strncat( str, buttonName, strlen(buttonName) );
+    strncat( str, "_PRESSED", strlen("_PRESSED") );
+
+    if ( strcmp(bleReceivedString, str) == 0 )  {
+        uartUsb.printf("Button '%s' has been pressed", buttonName );
+        uartUsb.printf("in the smartphone application\r\n\r\n");
+        if ( index >= 0 ) {
+            buttonsPressed[index] = 1;
+        }
+    }
+
+    str[0] = 0;
+    strncat( str, buttonName, strlen(buttonName) );
+    strncat( str, "_RELEASED", strlen("_RELEASED") );
+
+    if ( strcmp(bleReceivedString, str) == 0 )  {
+        if ( index >= 0 ) {
+            buttonsPressed[index] = 0;
+        }
+        uartUsb.printf("Button '%s' has been released", buttonName );
+        uartUsb.printf("in the smartphone application\r\n\r\n");
+    }
+}
+
+float analogReadingScaledWithTheLM35Formula( float analogReading )
+{
+	return ( analogReading * 3.3 / 0.01 );
+}
+
+float celsiusToFahrenheit( float tempInCelsiusDegrees )
+{
+	return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
+}
---
+++ programs/chapter_04/underHood4.cpp
@@ -0,0 +1,194 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+#define NUMBER_OF_BITS 12
+#define MAX_RESOLUTION 4095.0 //2^NUMBER_OF_BITS - 1
+
+//=====[Declaration and intitalization of public global objects]===============
+
+DigitalIn nextStepButton(BUTTON1);
+
+AnalogIn potentiometer(A0);
+
+DigitalOut startOfConversionLed(LED1);
+DigitalOut stepOfConversionLed(LED2);
+DigitalOut endOfConversionLed(LED3);
+
+Serial uartUsb(USBTX, USBRX);
+
+//=====[Declaration and intitalization of public global variables]=============
+
+bool comparatorOutput = 0;
+bool DACInput[NUMBER_OF_BITS];
+bool digitalOutput[NUMBER_OF_BITS];
+
+int conversionStep = 0;
+
+float analogInput;
+float digitalOutputScaledIntoRange0to1 = 0;
+float DACOutput = 0;
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void startOfConversion();
+void endOfConversion();
+bool analogComparator( );
+bool iterativeConversionControllerStep( );
+float digitalToAnalogConverter( );
+void resetIterativeConversionController( );
+void showConversionStatus( );
+void endOfConversion( );
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    inputsInit();
+    outputsInit();
+    while (true) {
+        startOfConversion();
+        resetIterativeConversionController();
+        for ( conversionStep = 1;
+              conversionStep <= NUMBER_OF_BITS;
+              conversionStep++) {
+
+            DACOutput = digitalToAnalogConverter( );
+
+            comparatorOutput = analogComparator( );
+
+            DACInput[NUMBER_OF_BITS - conversionStep] =
+                iterativeConversionControllerStep( );
+
+            showConversionStatus( );
+        }
+
+        endOfConversion( );
+    }
+}
+
+//=====[Implementations of public functions]===================================
+
+void inputsInit()
+{
+    //not neccesary
+}
+
+void outputsInit()
+{
+    startOfConversionLed = OFF;
+    stepOfConversionLed = OFF;
+    endOfConversionLed = OFF;
+}
+
+void startOfConversion()
+{
+    uartUsb.printf( "Please press nextStepButton\r\n\r\n" );
+    while (!nextStepButton) {};
+    uartUsb.printf( "Conversion started\r\n\r\n" );
+    analogInput = potentiometer.read();
+    startOfConversionLed = ON;
+    delay(1000);
+    startOfConversionLed = OFF;
+}
+
+void resetIterativeConversionController( )
+{
+    int i;
+    for ( i = 0; i < NUMBER_OF_BITS; i++) {
+        DACInput[i]=0;
+    }
+    uartUsb.printf( "DACinput reseted\r\n\r\n" );
+    startOfConversionLed = ON;
+    delay(1000);
+    startOfConversionLed = OFF;
+}
+
+bool iterativeConversionControllerStep( )
+{
+    if (comparatorOutput == 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+float digitalToAnalogConverter( )
+{
+    float output = 0;
+    int power = 1;
+    int i;
+
+    for (i=0; i<NUMBER_OF_BITS; i++) {
+        if (i == ( NUMBER_OF_BITS - conversionStep ) ) {
+            output += 1*power;
+        } else {
+            output += DACInput[i]*power;
+        }
+        power *= 2;
+    }
+
+    return output / MAX_RESOLUTION;
+}
+
+bool analogComparator( )
+{
+    if ( analogInput >= DACOutput ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void showConversionStatus( )
+{
+    int i;
+    uartUsb.printf( "Conversion step: %i\r\n", conversionStep );
+    uartUsb.printf( "Analog Input: %.3f\r\n", analogInput );
+    uartUsb.printf( "DAC Output: %.3f\r\n", DACOutput );
+    uartUsb.printf( "DAC Input: " );
+
+    for (i=1; i<=NUMBER_OF_BITS; i++) {
+        if (DACInput[NUMBER_OF_BITS-i] == 1) {
+            uartUsb.printf( "1" );
+        } else {
+            uartUsb.printf( "0" );
+        }
+    }
+    uartUsb.printf( "\r\n\r\n" );
+
+
+    delay(1000);
+    stepOfConversionLed = ON;
+    while (!nextStepButton);
+    stepOfConversionLed = OFF;
+}
+
+void endOfConversion( )
+{
+    int i;
+    
+    uartUsb.printf( "End of conversion\r\n\r\n" );
+
+    uartUsb.printf( "Analog Input: %.3f\r\n", analogInput );
+    uartUsb.printf( "DAC Output: %.3f\r\n", DACOutput );
+    uartUsb.printf( "DAC Input: " );
+
+    for (i=1; i<=NUMBER_OF_BITS; i++) {
+        if (DACInput[NUMBER_OF_BITS-i] == 1) {
+            uartUsb.printf( "1" );
+        } else {
+            uartUsb.printf( "0" );
+        }
+    }
+    uartUsb.printf( "\r\n\r\n" );
+    endOfConversionLed = ON;
+    delay(1000);
+    endOfConversionLed = OFF;
+}
