9b62723a9de909f8f6ffa25b860aa92b93080678
Eric Pernia <ericpernia@gmail.com>
2020-08-31 03:24:09 -0300

    update

--- programs/chapter_07/_display.h
+++ programs/chapter_07/_display.h
@@ -346,21 +346,113 @@ const char gasNotDetectedCustomCharBitmap[] = {
     0b00000
 };
 
-// Fire Alarm icon - Fire
-const char fireAlarmCustomCharBitmap[] = {
+// Fire Alarm icon 0 - Fire
+const char fireAlarmCustomCharBitmap0[] = {
+    0b00000,
+    0b00100,
+    0b01110,
+    0b01101,
+    0b10111,
+    0b11110,
+    0b01110,
+    0b00000,
+};
+
+// Fire Alarm icon 1 - Fire
+const char fireAlarmCustomCharBitmap1[] = {
     0b00100,
     0b00110,
-    0b01110,
+    0b01100,
     0b10110,
     0b11101,
     0b11111,
     0b01110,
-    0b00000
+    0b00000,
 };
+
+// Fire Alarm icon 2 - Fire
+const char fireAlarmCustomCharBitmap2[] = {
+    0b00100,
+    0b00000,
+    0b10101,
+    0b01111,
+    0b01110,
+    0b11111,
+    0b01110,
+    0b00000,
+};
+
+/*
+
+
+
+Funcionalidad Smart Home System hasta Chapter 6 -------------------------------
+
+https://docs.google.com/document/d/1hEsRCKBs3JFHS4ISZnKhub_Fq3hCdSUAFROuOshLWu0/edit
+
+LINKS LCD y GLCD --------------------------------------------------------------
+
+CAPSE Char LCD:
+
+https://docs.google.com/presentation/d/1FGUuhsB7eZqShnjHx24m0v6nBdULeNI2GgjxYNIFRGg/edit#slide=id.g2a10af01df_0_48
+
+
+Crear caracteres propios:
+
+https://maxpromer.github.io/LCD-Character-Creator/
+
+
+GLCD conexión arduino:
+
+https://saber.patagoniatec.com/2014/11/lcd-spi-128x64-serie-arduino-argentina-ptec-12864-grafico-glcd/
+
+https://www.instructables.com/id/Interface-GLCD-with-Arduino/
+
+GLCD Teoría y otros MCUs:
+
+https://monarcaelectronica.com.ar/productos/display-lcd-grafico-128x64-glcd-st7920-arduino-mona/?gclid=CjwKCAjwm_P5BRAhEiwAwRzSO_ZjrO-Ovpht7SSkjWkINKuntHiCVFhWpebjLM_qYfoE6PbswPiMPBoCfDAQAvD_BwE
+
+https://www.buydisplay.com/8051-microcontroller-development-board-for-graphic-lcd-erm12864-6
+
+https://www.electronicwings.com/8051/glcd-128x64-interfacing-with-at89s51-microcontroller
+
+https://www.electronicwings.com/avr-atmega/graphical-lcd-128x64-interfacing-with-atmega1632
+
+https://exploreembedded.com/wiki/Interfacing_GLCD(128x64)_with_PIC16F877A
+
+https://circuitdigest.com/microcontroller-projects/graphical-lcd-interfacing-with-arduino
+
+
+Teoria posicion de los caracteres en el GLCD:
+
+https://www.onetransistor.eu/2018/03/code-text-mode-st7920-arduino.html
+
+
+Libs Nucleo:
+
+https://github.com/cbm80amiga/ST7920_SPI
+
+https://controllerstech.com/glcd-128x64-st7920-interfacing-with-stm32/
+
+https://os.mbed.com/questions/52443/Using-Text-LCD-with-Nucleo-F401RE/
+
+
+Lib multiplataforma:
+
+https://github.com/olikraus/u8g2/wiki/Porting-to-new-MCU-platform#hardware-spi-communication
+
+
+Lib Arduino:
+
+
+
+*/
+
 
 // Example 7-4 GLCD por SPI usando modo texto ---------------------------------
 
-displayStatus_t displayInit( displayType_t type,
+displayStatus_t displayInit( display_t* display,
+                             displayType_t type,
                              displayConnection_t connection,
                              int charsPerLine, int charLines,
                              int charPixelWidth, int charPixelHeight,
