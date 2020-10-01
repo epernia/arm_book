c890beff5267008e5ad39fb47d5f3538f5c5c2ed
elpablogomez <elpablogomez@gmail.com>
2020-10-01 15:15:52 -0300

    se quitan algunos comentarios

--- programs/chapter_09/example_9_1/modules/wifi/esp8266/esp8266_at.cpp
+++ programs/chapter_09/example_9_1/modules/wifi/esp8266/esp8266_at.cpp
@@ -12,10 +12,6 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
-//static Serial uartEsp8266( USBTX, USBRX );
-
-// D42 = PE_8 = UART7_TX
-// D41 = PE_7 = UART7_RX
 static Serial uartEsp8266( D42, D41 );
 
 //=====[Declaration of external public global variables]=======================
@@ -33,8 +29,6 @@ static esp8266State_t esp8266State;
 //=====[Declarations (prototypes) of private functions]========================
 
 //=====[Implementations of public functions]===================================
-
-// UART used for the ESP8266 --------------------------------------------------
 
 void esp8266UartInit( int baudrate )
 {
@@ -63,17 +57,11 @@ void esp8266UartStringWrite( char const* str )
     }
 }
 
-// FSM Initialization and Update ----------------------------------------------
-
 void esp8266Init()
 {
     esp8266UartInit( ESP8266_BAUDRATE );
     esp8266State = ESP8266_IDLE;
 }
-
-// Tests AT startup. ----------------------------------------------------------
-
-// "AT\r\n"
 
 esp8266Status_t esp8266TestATSend()
 {
@@ -81,7 +69,7 @@ esp8266Status_t esp8266TestATSend()
         parserInit( &parser, "OK\r\n", strlen("OK\r\n"), 50 );
         parserStart( &parser );
         esp8266UartStringWrite( "AT\r\n" );
-        esp8266State = ESP8266_PROCESSING_AT_COMMAND; //ESP8266_PENDING_RESPONSE;
+        esp8266State = ESP8266_PROCESSING_AT_COMMAND;
         return ESP8266_AT_SENDED;
     } else {        
         return ESP8266_AT_NOT_SENDED;   
@@ -90,14 +78,11 @@ esp8266Status_t esp8266TestATSend()
 
 esp8266Status_t esp8266TestATResponse()
 {
-    // Receive from UART connected to ESP8266 if is data available
     char receivedChar = '\0';
     esp8266UartByteRead( &receivedChar );
     
-    // Update parser status
     parserStatus = parserPatternMatchOrTimeout( &parser, receivedChar );
 
-    // Chech parser response
     switch( parserStatus ) {
         case PARSER_TIMEOUT:
             esp8266State = ESP8266_IDLE;
--- programs/chapter_09/example_9_1/modules/wifi/esp8266/esp8266_at.h
+++ programs/chapter_09/example_9_1/modules/wifi/esp8266/esp8266_at.h
@@ -11,38 +11,29 @@
 
 //=====[Declaration of public data types]======================================
 
-// Global status
 typedef enum{   
-    ESP8266_IDLE,                        // Waiting for interactions
-    ESP8266_PROCESSING_AT_COMMAND,       // Sending AT command to ESP8266
+    ESP8266_IDLE,
+    ESP8266_PROCESSING_AT_COMMAND,
 }esp8266State_t;
 
-// Module status
 typedef enum{
-    ESP8266_AT_SENDED,                   // Module already send the command.  
-    ESP8266_AT_NOT_SENDED,               // Not sended.
+    ESP8266_AT_SENDED,
+    ESP8266_AT_NOT_SENDED,
 
-    ESP8266_AT_PENDING_RESPONSE,         // Waiting module response.  
-    ESP8266_AT_RESPONSED,                // Module already response.
-    ESP8266_AT_TIMEOUT_WAITING_RESPONSE, // Time-out waiting for a response.
+    ESP8266_AT_PENDING_RESPONSE,
+    ESP8266_AT_RESPONSED,
+    ESP8266_AT_TIMEOUT_WAITING_RESPONSE,
 }esp8266Status_t;
 
 //=====[Declarations (prototypes) of public functions]=========================
-
-// UART used for the ESP8266 --------------------------------------------------
 
 void esp8266UartInit( int baudrate );
 bool esp8266UartByteRead( char* receivedByte );
 void esp8266UartByteWrite( char sendedByte );
 void esp8266UartStringWrite( char const* str );
 
-// FSM Initialization and Update ----------------------------------------------
-
 void esp8266Init();
 
-// Tests AT startup. ----------------------------------------------------------
-
-// "AT\r\n"
 esp8266Status_t esp8266TestATSend();
 esp8266Status_t esp8266TestATResponse();
 
