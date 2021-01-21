035b3ae664442879d60ccd007142e902ebee9ac4
elpablogomez <elpablogomez@gmail.com>
2021-01-21 17:38:41 -0300

    se suben los test_program_8_2_1

---
+++ programs_v2/chapter_08/pruebas/test_program_8_2_1a_debug/modules/strobe_light/strobe_light.cpp
@@ -0,0 +1,62 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+#include "strobe_light.h"
+
+#include "smart_home_system.h"
+#include "alarm.h"
+
+//=====[Declaration of private defines]======================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut strobeLight(PD_10);
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+static bool strobeLightState = OFF;
+static int currentStrobeTime = 0;
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void strobeLightInit()
+{
+    //strobeLight.period(1.0f);
+    //trobeLight.write(0.0f);
+}
+
+bool strobeLightStateRead()
+{
+    return strobeLightState;
+}
+
+void strobeLightStateWrite( bool state )
+{
+    strobeLightState = state;
+}
+
+void strobeLightUpdate( int strobeTime )
+{
+    if( strobeLightState ) {
+        if (currentStrobeTime != strobeTime) {
+            //strobeLight.period( (float) strobeTime * 2 / 1000 );
+            //strobeLight.write(0.5f);
+            currentStrobeTime = strobeTime;
+        }
+    } else {
+        //strobeLight.write(0.0f);
+        currentStrobeTime = 0;
+    }
+}
+
+//=====[Implementations of private functions]==================================
---
+++ programs_v2/chapter_08/test_program_8_2_1a/modules/light_system/light_system.cpp
@@ -0,0 +1,98 @@
+//=====[Libraries]=============================================================
+
+#include "arm_book_lib.h"
+
+#include "light_system.h"
+#include "bright_control.h"
+#include "light_level_control.h"
+#include "ldr_sensor.h"
+
+//=====[Declaration of private defines]======================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut ledGreen(LED1);
+DigitalOut ledBlue(LED2);
+DigitalOut ledRed(LED3);
+
+//=====[Declaration and initialization of private global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+static float lightSystemLoopGain = 0.01;
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+static float dutyCycle = 0.0f;
+static bool brightnessRGBLedRedChangeEnabled = true;
+static bool brightnessRGBLedGreenChangeEnabled = false;
+static bool brightnessRGBLedBlueChangeEnabled = false;
+
+//=====[Implementations of public functions]===================================
+
+void lightSystemInit()
+{
+    brightControlInit();
+    ledRed = ON;
+    ledGreen = OFF;
+    ledBlue = OFF;
+}
+
+void lightSystemUpdate()
+{
+   
+    if ( brightnessRGBLedRedChangeEnabled ) setDutyCycle( RGB_LED_RED, dutyCycle );
+    if ( brightnessRGBLedGreenChangeEnabled ) setDutyCycle( RGB_LED_GREEN, dutyCycle );
+    if ( brightnessRGBLedBlueChangeEnabled ) setDutyCycle( RGB_LED_BLUE, dutyCycle );
+
+    dutyCycle = dutyCycle + 0.005f;
+    
+    if ( dutyCycle >= 0.99f ) {
+        dutyCycle = 0.0f;
+        if ( brightnessRGBLedRedChangeEnabled ) {
+            brightnessRGBLedRedChangeEnabled = false;
+            brightnessRGBLedGreenChangeEnabled = true;
+            brightnessRGBLedBlueChangeEnabled = false;
+            ledRed = OFF;
+            ledGreen = ON;
+            ledBlue = OFF;
+        } else if ( brightnessRGBLedGreenChangeEnabled ) {
+            brightnessRGBLedRedChangeEnabled = false;
+            brightnessRGBLedGreenChangeEnabled = false;
+            brightnessRGBLedBlueChangeEnabled = true;
+            ledRed = OFF;
+            ledGreen = OFF;
+            ledBlue = ON;
+        } else if ( brightnessRGBLedBlueChangeEnabled ) {
+            brightnessRGBLedRedChangeEnabled = true;
+            brightnessRGBLedGreenChangeEnabled = false;
+            brightnessRGBLedBlueChangeEnabled = false;
+            ledRed = ON;
+            ledGreen = OFF;
+            ledBlue = OFF;
+        }
+    }
+}
+
+void lightSystemBrightnessChangeEnable( lightSystem_t light, bool state )
+{
+    switch( light ) {
+        case RGB_LED_RED:
+            brightnessRGBLedRedChangeEnabled = state;
+        break;
+        case RGB_LED_GREEN:
+            brightnessRGBLedGreenChangeEnabled = state;
+        break;
+        case RGB_LED_BLUE:
+            brightnessRGBLedBlueChangeEnabled = state;
+        break;
+        default:
+        break;
+    }
+}
+
+//=====[Implementations of private functions]==================================
