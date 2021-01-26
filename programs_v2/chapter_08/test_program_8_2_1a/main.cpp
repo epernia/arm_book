b7b9671627e506b918daa48e2907541fc70ccd11
elpablogomez <elpablogomez@gmail.com>
2021-01-26 11:34:41 -0300

    otra versión del test_8_2_1

--- programs_v2/chapter_08/test_program_8_2_1a/main.cpp
+++ programs_v2/chapter_08/test_program_8_2_1a/main.cpp
@@ -1,13 +1,68 @@
 //=====[Libraries]=============================================================
 
-#include "smart_home_system.h"
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]=============================================================
+
+//=====[Declaration of public data types]======================================
+
+typedef enum {
+    RGB_LED_RED,
+    RGB_LED_GREEN,
+    RGB_LED_BLUE,
+} lightSystem_t;
+
+//=====[Declaration and initialization of public global objects]===============
+
+//DigitalOut ledGreen(LED1);
+//DigitalOut ledBlue(LED2);
+//DigitalOut ledRed(LED3);
+
+PwmOut RGBLed[] = {(PB_4), (PA_0), (PD_12)};
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void setPeriod( lightSystem_t light, float period );
+void setDutyCycle( lightSystem_t light, float dutyCycle );
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    smartHomeSystemInit();
+    setPeriod( RGB_LED_RED, 0.01f );
+    setPeriod( RGB_LED_GREEN, 0.01f );
+    setPeriod( RGB_LED_BLUE, 0.01f );
+
+    setDutyCycle( RGB_LED_RED, 0.0f );
+    setDutyCycle( RGB_LED_GREEN, 0.0f );
+    setDutyCycle( RGB_LED_BLUE, 0.0f );
+    
     while (true) {
-        smartHomeSystemUpdate();
+        for (float x = 0.00; x < 0.99; x += 0.005) {
+            for (float y = 0.00; y < 0.99; y += 0.005) {
+                for (float z = 0.00; z < 0.99; z += 0.005) {
+                    setDutyCycle( RGB_LED_RED, z );
+                }
+                setDutyCycle( RGB_LED_GREEN, y );
+            }
+            setDutyCycle( RGB_LED_BLUE, x );
+        }
     }
+}
+
+//=====[Implementations of public functions]===================================
+
+void setDutyCycle( lightSystem_t light, float dutyCycle )
+{
+    RGBLed[light].write(dutyCycle);
+}
+
+//=====[Implementations of private functions]==================================
+
+void setPeriod( lightSystem_t light, float period )
+{
+    RGBLed[light].period(period);
 }
