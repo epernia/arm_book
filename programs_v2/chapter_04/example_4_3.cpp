de2b513442c70dca84d87b836064b2d81445d27d
elpablogomez <elpablogomez@gmail.com>
2021-01-28 22:54:23 -0300

    restarué el example 4-3 que lo había cambiado por error

--- programs_v2/chapter_04/example_4_3.cpp
+++ programs_v2/chapter_04/example_4_3.cpp
@@ -3,112 +3,444 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-#include "FATFileSystem.h"
-#include "SDBlockDevice.h"
+//=====[Defines]===============================================================
 
-#include "platform/mbed_retarget.h"
-
-//=====[Defines]=============================================================
-
-#define SPI3_MOSI   PC_12
-#define SPI3_MISO   PC_11
-#define SPI3_SCK    PC_10
-#define SPI3_CS     PA_4_ALT0
+#define NUMBER_OF_KEYS                           4
+#define BLINKING_TIME_GAS_ALARM               1000
+#define BLINKING_TIME_OVER_TEMP_ALARM          500
+#define BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM  100
+#define NUMBER_OF_AVG_SAMPLES                   100
+#define OVER_TEMP_LEVEL                         50
+#define TIME_INCREMENT_MS                       10
+#define DEBOUNCE_KEY_TIME_MS                    40
+#define KEYPAD_NUMBER_OF_ROWS                    4
+#define KEYPAD_NUMBER_OF_COLS                    4
 
 //=====[Declaration of public data types]======================================
 
+typedef enum {
+    MATRIX_KEYPAD_SCANNING,
+    MATRIX_KEYPAD_DEBOUNCE,
+    MATRIX_KEYPAD_KEY_HOLD_PRESSED
+} matrixKeypadState_t;
+
 //=====[Declaration and initialization of public global objects]===============
 
-SDBlockDevice sd( SPI3_MOSI, SPI3_MISO, SPI3_SCK, SPI3_CS );
+DigitalIn alarmTestButton(BUTTON1);
+DigitalIn mq2(PE_12);
 
-FATFileSystem fs("sd", &sd);
+DigitalOut alarmLed(LED1);
+DigitalOut incorrectCodeLed(LED3);
+DigitalOut systemBlockedLed(LED2);
+
+DigitalInOut sirenPin(PE_10);
 
 Serial uartUsb(USBTX, USBRX);
 
+AnalogIn lm35(A1);
+
+DigitalOut keypadRowPins[KEYPAD_NUMBER_OF_ROWS] = {PB_3, PB_5, PC_7, PA_15};
+DigitalIn keypadColPins[KEYPAD_NUMBER_OF_COLS]  = {PB_12, PB_13, PB_15, PC_6};
+
 //=====[Declaration and initialization of public global variables]=============
 
+bool alarmState    = OFF;
+bool incorrectCode = false;
+bool overTempDetector = OFF;
+
+int numberOfIncorrectCodes = 0;
+int numberOfHashKeyReleasedEvents = 0;
+int keyBeingCompared    = 0;
+char codeSequence[NUMBER_OF_KEYS]   = { '1', '8', '0', '5' };
+char keyPressed[NUMBER_OF_KEYS] = { '0', '0', '0', '0' };
+int accumulatedTimeAlarm = 0;
+
+int lm35SampleIndex      = 0;
+
+char receivedChar = '\0';
+bool gasDetectorState          = OFF;
+bool overTempDetectorState     = OFF;
+
+float potentiometerReading = 0.0;
+float lm35ReadingsAverage  = 0.0;
+float lm35ReadingsSum      = 0.0;
+float lm35ReadingsArray[NUMBER_OF_AVG_SAMPLES];
+float lm35TempC            = 0.0;
+
+int accumulatedDebounceMatrixKeypadTime = 0;
+int matrixKeypadCodeIndex = 0;
+char matrixKeypadLastKeyPressed = '\0';
+char matrixKeypadIndexToCharArray[] = {
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D',
+};
+matrixKeypadState_t matrixKeypadState;
+
 //=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void alarmActivationUpdate();
+void alarmDeactivationUpdate();
+
+void uartTask();
+void availableCommands();
+bool areEqual();
+float celsiusToFahrenheit( float tempInCelsiusDegrees );
+float analogReadingScaledWithTheLM35Formula( float analogReading );
+void lm35ReadingsArrayInit();
+
+void matrixKeypadInit();
+char matrixKeypadScan();
+char matrixKeypadUpdate();
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    uartUsb.printf("Looking for a filesystem in the SD card... \r\n");
-    fs.mount(&sd);
-    DIR *dir = opendir("/sd/");
-    if ( dir != NULL ) {
-        uartUsb.printf("Filesystem found in the SD card. \r\n");
-        closedir(dir);
-        return true;
-    } else {
-        uartUsb.printf("Filesystem not mounted. \r\n");
-        uartUsb.printf("Insert an SD card and ");
-        uartUsb.printf("reset the NUCLEO board.\r\n");
-        return false;
-    }
-    
+    inputsInit();
+    outputsInit();
     while (true) {
-        
-        if( uartUsb.readable() ) {
-            char receivedChar = uartUsb.getc();
-            switch (receivedChar) {
-            case 'w':
-            
-                char fileNameSD[80];
-                
-                fileNameSD[0] = 0;
-                strncat( fileNameSD, "/sd/", strlen("/sd/") );
-                strncat( fileNameSD, "Hello.txt", strlen("Hello.txt") );
-
-                FILE *fd = fopen( fileNameSD, "a" );
-
-                if ( fd != NULL ) {
-                    fprintf( fd, "%s", "Hello World!" );                       
-                    fclose( fd );
-                    return true;
-                } else {
-                    return false;
-                }
-                
-                break;
-
-            case 'l':
-            
-                int NumberOfUsedBytesInBuffer = 0;
-                struct dirent *de;
-
-                DIR *dir = opendir("/sd/");
-
-                if ( dir != NULL ) {
-                    uartUsb.printf("Printing all filenames:\r\n");
-                    de = readdir(dir);
-                    
-                    while ( de != NULL ) {
-                                    
-                        uartUsb.printf( de->d_name );
-                        uartUsb.printf("\r\n");
-
-                        de = readdir(dir);
-                    }
-                    
-                    closedir(dir);
-                    uartUsb.printf("\r\n");
-                    
-                    return true;
-                } else {
-                    uartUsb.printf("Insert an SD card and ");
-                    uartUsb.printf("reset the board.\r\n");
-                    return false;
-                }
-                break;
-    
-            default:
-
-                break;
-                
-            }
-        }
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
+        uartTask();
+        delay(TIME_INCREMENT_MS);
     }
 }
 
 //=====[Implementations of public functions]===================================
+
+void inputsInit()
+{
+    lm35ReadingsArrayInit();
+    alarmTestButton.mode(PullDown);
+    sirenPin.mode(OpenDrain);
+    sirenPin.input();
+    matrixKeypadInit();
+}
+
+void outputsInit()
+{
+    alarmLed = OFF;
+    incorrectCodeLed = OFF;
+    systemBlockedLed = OFF;
+}
+
+void alarmActivationUpdate()
+{
+    static int lm35SampleIndex = 0;
+    int i = 0;
+
+    lm35ReadingsArray[lm35SampleIndex] = lm35.read();
+    lm35SampleIndex++;
+    if ( lm35SampleIndex >= NUMBER_OF_AVG_SAMPLES) {
+        lm35SampleIndex = 0;
+    }
+    
+    lm35ReadingsSum = 0.0;
+    for (i = 0; i < NUMBER_OF_AVG_SAMPLES; i++) {
+        lm35ReadingsSum = lm35ReadingsSum + lm35ReadingsArray[i];
+    }
+    lm35ReadingsAverage = lm35ReadingsSum / NUMBER_OF_AVG_SAMPLES;
+       lm35TempC = analogReadingScaledWithTheLM35Formula ( lm35ReadingsAverage );    
+    
+    if ( lm35TempC > OVER_TEMP_LEVEL ) {
+        overTempDetector = ON;
+    } else {
+        overTempDetector = OFF;
+    }
+
+    if( !mq2) {                      
+        gasDetectorState = ON;
+        alarmState = ON;
+    }
+    if( overTempDetector ) {
+        overTempDetectorState = ON;
+        alarmState = ON;
+    }
+    if( alarmTestButton ) {             
+        overTempDetectorState = ON;
+        gasDetectorState = ON;
+        alarmState = ON;
+    }    
+    if( alarmState ) { 
+        accumulatedTimeAlarm = accumulatedTimeAlarm + TIME_INCREMENT_MS;
+        sirenPin.output();                                     
+        sirenPin = LOW;                                        
+    
+        if( gasDetectorState && overTempDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM ) {
+                accumulatedTimeAlarm = 0;
+                alarmLed = !alarmLed;
+            }
+        } else if( gasDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_ALARM ) {
+                accumulatedTimeAlarm = 0;
+                alarmLed = !alarmLed;
+            }
+        } else if ( overTempDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_OVER_TEMP_ALARM  ) {
+                accumulatedTimeAlarm = 0;
+                alarmLed = !alarmLed;
+            }
+        }
+    } else{
+        alarmLed = OFF;
+        gasDetectorState = OFF;
+        overTempDetectorState = OFF;
+        sirenPin.input();                                  
+    }
+}
+
+void alarmDeactivationUpdate()
+{
+    if ( numberOfIncorrectCodes < 5 ) {
+        char keyReleased = matrixKeypadUpdate();
+        if( keyReleased != '\0' && keyReleased != '#' ) {
+            keyPressed[matrixKeypadCodeIndex] = keyReleased;
+            if( matrixKeypadCodeIndex >= NUMBER_OF_KEYS ) {
+                matrixKeypadCodeIndex = 0;
+            } else {
+                matrixKeypadCodeIndex++;
+            }
+        }
+        if( keyReleased == '#' ) {
+            if( incorrectCodeLed ) {
+                numberOfHashKeyReleasedEvents++;
+                if( numberOfHashKeyReleasedEvents >= 2 ) {
+                    incorrectCodeLed = OFF;
+                    numberOfHashKeyReleasedEvents = 0;
+                    matrixKeypadCodeIndex = 0;
+                }
+            } else {
+                if ( alarmState ) {
+                    if ( areEqual() ) {
+                        alarmState = OFF;
+                        numberOfIncorrectCodes = 0;
+                        matrixKeypadCodeIndex = 0;
+                    } else {
+                        incorrectCodeLed = ON;
+                        numberOfIncorrectCodes++;
+                    }
+                }
+            }
+        }
+    } else {
+        systemBlockedLed = ON;
+    }
+}
+
+void uartTask()
+{
+    if( uartUsb.readable() ) {
+        char receivedChar = uartUsb.getc();
+        switch (receivedChar) {
+        case '1':
+            if ( alarmState ) {
+                uartUsb.printf( "The alarm is activated\r\n");
+            } else {
+                uartUsb.printf( "The alarm is not activated\r\n");
+            }
+            break;
+
+        case '2':
+            if ( !mq2 ) {
+                uartUsb.printf( "Gas is being detected\r\n");
+            } else {
+                uartUsb.printf( "Gas is not being detected\r\n");
+            }
+            break;
+
+        case '3':
+            if ( overTempDetector ) {
+                uartUsb.printf( "Temperature is above the maximum level\r\n");
+            } else {
+                uartUsb.printf( "Temperature is below the maximum level\r\n");
+            }
+            break;
+
+        case '4':
+            uartUsb.printf( "Please enter the four digits numeric code " );
+            uartUsb.printf( "to deactivate the alarm: " );
+
+            incorrectCode = false;
+
+            for ( keyBeingCompared = 0;
+                  keyBeingCompared < NUMBER_OF_KEYS;
+                  keyBeingCompared++) {
+                receivedChar = uartUsb.getc();
+                uartUsb.printf( "*" );
+                if ( codeSequence[keyBeingCompared] != receivedChar ) {
+                    incorrectCode = true;
+                }
+            }
+
+            if ( incorrectCode == false ) {
+                uartUsb.printf( "\r\nThe code is correct\r\n\r\n" );
+                alarmState = OFF;
+                incorrectCodeLed = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                uartUsb.printf( "\r\nThe code is incorrect\r\n\r\n" );
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
+            }
+            break;
+
+        case '5':
+            uartUsb.printf( "Please enter the new four digits numeric code " );
+            uartUsb.printf( "to deactivate the alarm: " );
+
+            for ( keyBeingCompared = 0;
+                  keyBeingCompared < NUMBER_OF_KEYS;
+                  keyBeingCompared++) {
+                codeSequence[keyBeingCompared] = uartUsb.getc();
+                uartUsb.printf( "*" );
+            }
+
+            uartUsb.printf( "\r\nNew code configured\r\n\r\n" );
+            break;
+
+        case 'c':
+        case 'C':
+            uartUsb.printf( "Temperature: %.2f ºC\r\n", lm35TempC );
+            break;
+
+        case 'f':
+        case 'F':
+            uartUsb.printf( "Temperature: %.2f ºF\r\n", 
+                celsiusToFahrenheit( lm35TempC ) );
+            break;
+
+        default:
+            availableCommands();
+            break;
+
+        }
+    }
+}
+
+void availableCommands()
+{
+    uartUsb.printf( "Available commands:\r\n" );
+    uartUsb.printf( "Press '1' to get the alarm state\r\n" );
+    uartUsb.printf( "Press '2' for gas detector state\r\n" );
+    uartUsb.printf( "Press '3' for over temperature detector state\r\n" );
+    uartUsb.printf( "Press '4' to enter the code sequence\r\n" );
+    uartUsb.printf( "Press '5' to enter a new code\r\n" );
+    uartUsb.printf( "Press 'P' or 'p' to get potentiometer reading\r\n" );
+    uartUsb.printf( "Press 'f' or 'F' to get lm35 reading in Fahrenheit\r\n" );
+    uartUsb.printf( "Press 'c' or 'C' to get lm35 reading in Celsius\r\n\r\n" );
+}
+
+bool areEqual()
+{
+    int i;
+
+    for (i = 0; i < NUMBER_OF_KEYS; i++) {
+        if (codeSequence[i] != keyPressed[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+float analogReadingScaledWithTheLM35Formula( float analogReading )
+{
+    return ( analogReading * 3.3 / 0.01 );
+}
+
+float celsiusToFahrenheit( float tempInCelsiusDegrees )
+{
+    return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
+}
+void lm35ReadingsArrayInit()
+{
+    int i;
+    for( i=0; i<NUMBER_OF_AVG_SAMPLES ; i++ ) {
+        lm35ReadingsArray[i] = 0;
+    }
+}
+
+void matrixKeypadInit()
+{
+    matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+    int pinIndex = 0;
+    for( pinIndex=0; pinIndex<KEYPAD_NUMBER_OF_COLS; pinIndex++ ) {
+        (keypadColPins[pinIndex]).mode(PullUp);
+    }
+}
+
+char matrixKeypadScan()
+{
+    int r = 0;
+    int c = 0;
+    int i = 0;
+
+    for( r=0; r<KEYPAD_NUMBER_OF_ROWS; r++ ) {
+
+        for( i=0; i<KEYPAD_NUMBER_OF_ROWS; i++ ) {
+            keypadRowPins[i] = ON;
+        }
+
+        keypadRowPins[r] = OFF;
+
+        for( c=0; c<KEYPAD_NUMBER_OF_COLS; c++ ) {
+            if( keypadColPins[c] == OFF ) {
+                return matrixKeypadIndexToCharArray[r*KEYPAD_NUMBER_OF_ROWS + c];
+            }
+        }
+    }
+    return '\0';
+}
+
+char matrixKeypadUpdate()
+{
+    char keyDetected = '\0';
+    char keyReleased = '\0';
+
+    switch( matrixKeypadState ) {
+
+    case MATRIX_KEYPAD_SCANNING:
+        keyDetected = matrixKeypadScan();
+        if( keyDetected != '\0' ) {
+            matrixKeypadLastKeyPressed = keyDetected;
+            accumulatedDebounceMatrixKeypadTime = 0;
+            matrixKeypadState = MATRIX_KEYPAD_DEBOUNCE;
+        }
+        break;
+
+    case MATRIX_KEYPAD_DEBOUNCE:
+        if( accumulatedDebounceMatrixKeypadTime >=
+            DEBOUNCE_KEY_TIME_MS ) {
+            keyDetected = matrixKeypadScan();
+            if( keyDetected == matrixKeypadLastKeyPressed ) {
+                matrixKeypadState = MATRIX_KEYPAD_KEY_HOLD_PRESSED;
+            } else {
+                matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+            }
+        }
+        accumulatedDebounceMatrixKeypadTime =
+            accumulatedDebounceMatrixKeypadTime + TIME_INCREMENT_MS;
+        break;
+
+    case MATRIX_KEYPAD_KEY_HOLD_PRESSED:
+        keyDetected = matrixKeypadScan();
+        if( keyDetected != matrixKeypadLastKeyPressed ) {
+            if( keyDetected == '\0' ) {
+                keyReleased = matrixKeypadLastKeyPressed;
+            }
+            matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+        }
+        break;
+
+    default:
+        matrixKeypadInit();
+        break;
+    }
+    return keyReleased;
+}
