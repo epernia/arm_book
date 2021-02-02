528dd9330799ab88cb83dc58c9860bda8314fb3e
elpablogomez <elpablogomez@gmail.com>
2021-02-02 07:44:00 -0300

    test_program_10_2_1

---
+++ programs_v2/chapter_10/test_program_10_2_1/modules/sd_card/sd_card.h
@@ -0,0 +1,24 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SD_CARD_H_
+#define _SD_CARD_H_
+
+//=====[Libraries]=============================================================
+
+#define SD_CARD_FILENAME_MAX_LENGTH 32
+
+//=====[Declaration of public defines]=======================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+bool sdCardInit();
+bool sdCardWriteFile( const char* fileName, const char* writeBuffer );
+bool sdCardListFiles( char* fileNamesBuffer, int fileNamesBufferSize );
+bool sdCardReadFile( const char * fileName, char * readBuffer );
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _SD_CARD_H_
---
+++ programs_v2/chapter_10/test_program_10_2_1/modules/smart_home_system/smart_home_system.cpp
@@ -0,0 +1,66 @@
+//=====[Libraries]=============================================================
+
+#include "arm_book_lib.h"
+
+#include "smart_home_system.h"
+
+#include "alarm.h"
+#include "user_interface.h"
+#include "fire_alarm.h"
+#include "intruder_alarm.h"
+#include "pc_serial_com.h"
+#include "event_log.h"
+#include "motion_sensor.h"
+#include "motor.h"
+#include "gate.h"
+#include "light_system.h"
+#include "audio.h"
+#include "sd_card.h"
+#include "ble_com.h"
+
+//=====[Declaration of private defines]======================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+char systemBuffer[EVENT_STR_LENGTH*EVENT_LOG_MAX_STORAGE];
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void smartHomeSystemInit()
+{
+    audioInit();
+    userInterfaceInit();
+    alarmInit();
+    fireAlarmInit();
+    intruderAlarmInit();
+    pcSerialComInit();
+    motorControlInit();
+    gateInit();
+    lightSystemInit();
+    sdCardInit();
+}
+
+void smartHomeSystemUpdate()
+{
+    userInterfaceUpdate();
+    fireAlarmUpdate();
+    intruderAlarmUpdate();
+    alarmUpdate();
+    eventLogUpdate();
+    pcSerialComUpdate();
+    lightSystemUpdate();
+    bleComUpdate();
+    delay(SYSTEM_TIME_INCREMENT_MS);
+}
+
+//=====[Implementations of private functions]==================================
