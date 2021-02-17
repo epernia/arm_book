39f024b9240530c6296ca455decbd9010be5cfce
elpablogomez <elpablogomez@gmail.com>
2021-02-17 11:15:06 -0300

    nueva versión de arm_book_lib

--- programs_v2/arm_book_lib.h
+++ programs_v2/arm_book_lib.h
@@ -1,45 +1,10 @@
-/* Copyright 2020, Eric Pernia, Pablo Gomez and Ariel Lutemberg.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef _ARM_BOOK_LIBRARY_H_
 #define _ARM_BOOK_LIBRARY_H_
 
-/*==================[inclusions]=============================================*/
+/*==================[Libraries]=============================================*/
 
 #include <cstdint>
-#include <mbed.h> // [Libro ARM] Si lo ponemos en todos los archivos de examples se puede sacar de aca
-
-/*==================[c++]====================================================*/
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <mbed.h> 
 
 /*==================[macros]=================================================*/
 
@@ -56,7 +21,7 @@ extern "C" {
 #define LOW    0
 #endif
 #ifndef HIGH
-#define HIGH   (!LOW) // [Libro ARM] Los usamos creo que solamente en lo del motor, evaluemos sacarlos
+#define HIGH   (!LOW)
 #endif
 
 // Logical states
@@ -79,12 +44,6 @@ extern "C" {
 
 #define delay(ms)      thread_sleep_for( ms )
 
-/*==================[typedef]================================================*/
-
-/*==================[c++]====================================================*/
-#ifdef __cplusplus
-}
-#endif
-
 /*==================[end of file]============================================*/
+
 #endif // _ARM_BOOK_LIBRARY_H_
